8425fc179b0acc82e786f2672e184175eb92c3b8
nicolasbuscarini <nicolasbuscarini@hotmail.com>
2022-10-05 12:17:25 -0300

    correcao tabela resultado adicionando dicionario de dados

--- Service/bot_orcamento.py
+++ Service/bot_orcamento.py
@@ -14,7 +14,11 @@ class BotOrcamento:
         self.FILIAL = filial
         self.DICIONARIO_PRODUTOS = dicionario_produtos
         self.planilha = Planilha() 
-        self.anterior = 'null'
+
+        self.produto_atual = 'null'
+        self.descricao_atual = 'null'
+        self.produto_anterior = 'null'
+        self.descricao_anterior = 'null'
 
     def initialize(self):
         '''
@@ -61,27 +65,30 @@ class BotOrcamento:
 
     def format_nome_produto(self, nome_produto : str, caracteres_indesejados : str):
         '''
+            Remover caracteres indesejados
             Concatena o nome do produto com '%' entre as palavras para realizar pesquisa\n
             Transforma em UPPER CASE\n
-            Tenta converter cada palavra do produto para o nome do produto no sistema
         '''
         nome_produto = self.remover_caracteres_indesejados(nome_produto, caracteres_indesejados)
 
         nome_produto = nome_produto.upper()
 
-        # Tenta converter cada palavra do produto para o nome do produto no sistema
-        lista_palavras = nome_produto.split(' ')
-        for idx, palavra in enumerate(lista_palavras):
-            resultado_dicionario = self.DICIONARIO_PRODUTOS.get(palavra)
-            lista_palavras[idx] = resultado_dicionario
+        nome_produto_concatenado = nome_produto.replace(" ", "%")
 
-        nome_produto_concatenado = '%'.join(lista_palavras)
         return nome_produto_concatenado
+
+    def converter_dicionario(self, produto: str) :
+        ''''
+            converte com o dicionario de nome de produtos
+        '''
+        resultado_dicionario = self.DICIONARIO_PRODUTOS.get(produto)
+        return resultado_dicionario
 
     def pesquisar_produto(self, produto : str, caracteres_indesejados : str):
         '''
             Pesquisa o produto na tela do sistema
         '''
+        self.converter_dicionario(produto)
         nome_produto_concatenado = self.format_nome_produto(produto, caracteres_indesejados)
         
         # inputProduto
@@ -108,7 +115,6 @@ class BotOrcamento:
 
             self.pesquisar_produto(linha[0], self.CARACTERES_INDESEJADOS)
             self.selecionar_campo_descricao()
-            pyautogui.hotkey('left')
 
             if self.verificar_busca(linha[0], linha[1]):
                 print(f'Produto {linha[0]} encontrado!')
@@ -128,20 +134,23 @@ class BotOrcamento:
         '''
             Função recursiva que verifica se o produto pesquisado é o mesmo que está na planilha
         '''
-        produto = self.get_clipboard()
+        self.descricao_atual = self.get_clipboard()
+        pyautogui.hotkey('left')
+        self.produto_atual = self.get_clipboard()
         
-        if produto == self.anterior or produto == '' or produto == NULL:
+        if self.produto_atual == self.produto_anterior or self.produto_atual == '' or self.produto_atual == NULL:
             time.sleep(self.TEMPO_ENTRE_ACOES)
             
-            self.planilha.adicionar_dados_tabela_resultado(produto, qtd_planilha, False)
+            self.planilha.adicionar_dados_tabela_resultado(produto_planilha, qtd_planilha, False)
             return False
         
-        if produto != self.anterior :
+        if self.produto_atual != self.produto_anterior :
             time.sleep(self.TEMPO_ENTRE_ACOES)
             self.selecionar_produto()
-            self.anterior = produto
+            self.produto_anterior = self.produto_atual
+            self.descricao_anterior = self.descricao_atual
 
-            self.planilha.adicionar_dados_tabela_resultado(produto, qtd_planilha, True)
+            self.planilha.adicionar_dados_tabela_resultado(produto_planilha, qtd_planilha, True)
             return True
             
         pyautogui.hotkey('down')
