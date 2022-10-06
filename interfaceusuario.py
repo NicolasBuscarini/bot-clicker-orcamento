881ca3a6eab2e9f33f74fde915d1f12eb0329791
Nicolas <nicolasbuscarini@hotmail.com>
2022-10-06 00:12:32 -0300

    scopes

--- interfaceusuario.py
+++ interfaceusuario.py
@@ -1,24 +1,47 @@
 # A simple script to calculate BMI
 from pywebio.input import input as inp, NUMBER, file_upload
-from pywebio.output import put_text
+from pywebio.output import put_text, clear, toast, use_scope
 from Util.json_util import Json
 
 
 class InterfaceUsuario:
 
     @staticmethod
-    def initial():
+    def initial(*, error_message=None):
         """Initial interface"""
         json = Json('Config/config.json')
-        filial = inp("Digite a FILIAL：", type=NUMBER)
-        json.set('filial', filial)
-        json.write()
+        InterfaceUsuario.ask_filial(json)
 
-        f = file_upload("Upload a file")
-        open('asset/' + f['filename'], 'wb').write(f['content'])
+        InterfaceUsuario.ask_file()
 
         put_text("O bot irá iniciar... Não utilize seu teclado nem mouse até que o bot termine.")
         put_text("Para interromper o bot mova o mouse para um dos cantos da tela.")
+
+    @staticmethod
+    def ask_filial(json: Json):
+        """Ask filial"""
+        with use_scope('scope_filial'):
+            filial = inp("Digite a FILIAL：", type=NUMBER)
+            if filial is None:
+                clear("scope_filial")
+                toast("Filial incorreto.", position="center", color="error")
+                InterfaceUsuario.ask_filial(json)
+                return
+            json.set('filial', filial)
+            json.write()
+
+    @staticmethod
+    def ask_file():
+        """Ask for file"""
+        with use_scope('scope_file'):
+            f = file_upload("Upload a file", accept=".xlsx")
+
+            if not f or not f['filename'].endswith(".xlsx"):
+                clear("scope_file")
+                toast("File not found.", position="center", color="error")
+                InterfaceUsuario.ask_file()
+                return
+            open('Excel/Execucao.xlsx', 'wb').write(f['content'])
 
     @staticmethod
     def error(e: Exception):
